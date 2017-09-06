a997b19695b4cd0885a33fb0477227474f0e47dd
Juan Patricio Di Bacco <jpdibacco@gmail.com>
2017-09-06 02:26:53 +0000

    added php file for server msgs

---
+++ aula.php
@@ -0,0 +1,41 @@
+
+<?php
+// API access key from Google API's Console
+define( 'API_ACCESS_KEY', 'AAAANWsRzkU:APA91bGCxXxw04W7KAcFWngbPUcSeIebj8PUefcFK11e8E41Fht8J25dOgZqorv0LvK1Iv2k8vjp8X0fro04YTyqMzAuMjyPs3FoniakyyM_IFr8_B9vdRCXtGDJxO3o0wTrKC-7xpfA' );
+$registrationIds = array( $_GET['id'] );
+//$registrationIds = '229429595717';
+// prep the bundle
+$msg = array
+(
+	'message' 	=> 'here is a message. message',
+	'title'		=> 'This is a title. title',
+	'subtitle'	=> 'This is a subtitle. subtitle',
+	'tickerText'	=> 'Ticker text here...Ticker text here...Ticker text here',
+	'vibrate'	=> 1,
+	'sound'		=> 1,
+	'largeIcon'	=> 'large_icon',
+	'smallIcon'	=> 'small_icon'
+);
+$fields = array
+(
+	//'registration_ids' 	=> $registrationIds,
+  'to'  => '/topics/alumnos',
+	'data'			=> $msg
+);
+
+$headers = array
+(
+	'Authorization: key=' . API_ACCESS_KEY,
+	'Content-Type: application/json'
+);
+
+$ch = curl_init();
+curl_setopt( $ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send' );
+curl_setopt( $ch,CURLOPT_POST, true );
+curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+$result = curl_exec($ch );
+curl_close( $ch );
+echo $result;
